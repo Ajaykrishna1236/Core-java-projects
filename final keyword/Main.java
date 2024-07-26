2aa0673276f19f1ad9139dc98ddffc0df8c81ebd
Ajay <Ajaykrishnachnr@gmail.com>
2024-07-26 23:55:12 +0530

    final keyword task completed

---
+++ final keyword/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+	public static void main(String []args){
+	 // to apply final keyword in java
+	 
+	final int x=10;
+	final boolean empty=false;
+	
+	final double value=3.14;
+	final char d='@';
+	// final keyword is cannot change the value from old variable
+	
+	boolean empty=true;
+	
+	System.out.println(x);
+	System.out.println(empty);
+	System.out.println(value);
+	System.out.println(d);
+	 
+	 
+	}
+}
