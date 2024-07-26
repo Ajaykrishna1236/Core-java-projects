f0af0cc18a62dae339f24d39ac757873cbe07056
Ajay <Ajaykrishnachnr@gmail.com>
2024-07-26 23:28:13 +0530

    overloaded Method simple project

---
+++ overloaded Methods/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+	
+// create an overloaded methods
+
+// create an static methods
+
+static int add(int a,int b,int c,int d,int f){
+	
+	return a+b+c+d+f;
+}	
+
+public static void main(String[]args){
+	
+	int x=add(4,5,10,8,9);
+	
+	System.out.println(x);
+	
+}
+
+}
