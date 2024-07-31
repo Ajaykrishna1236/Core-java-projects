9a237aef93ea9ea8479c2de13304b55e214759de
Ajay <Ajaykrishnachnr@gmail.com>
2024-07-31 11:36:25 +0530

    toString method simple project completed

---
+++ toString()/oops basic project/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+public static void main(String []args){
+	
+	//toString():- special method that all objects inherit tha rerurns a string that textually represented an object,	can be used implicitly and explicitly.
+
+//crate an object Mobile 
+
+Mobile mobile = new Mobile();
+//call implicitly that is 
+System.out.println(mobile);
+
+System.out.println();
+
+// call explicitly
+System.out.println(mobile.toString());
+	
+
+	
+}
+
+}
---
+++ toString()/oops basic project/Mobile.java
@@ -0,0 +1,14 @@
+public class Mobile{
+
+// create an attribute of Mobile
+
+String colour="black";
+String brand="Samsung";
+String model= "Android";
+int price=40000;
+
+// to implement toString method
+
+
+
+}
