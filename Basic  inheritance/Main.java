ea0c4cb0f73b1fdc69e5b9df01297baca5288d01
Ajay <Ajaykrishnachnr@gmail.com>
2024-08-01 10:27:05 +0530

    basic inhertance project completed

---
+++ Basic  inheritance/Main.java
@@ -0,0 +1,26 @@
+public class Main{
+public static void main(String[]args){
+
+// inheritance is a process where one class acquires the attributes and methods of another
+
+
+
+// create an object of Car
+Car car = new Car();
+car.go();
+car.stopp();
+
+System.out.println(car.wheels);
+System.out.println(car.speed);
+
+
+//create bike object 
+
+Bike bike = new Bike();
+bike.go();
+bike.stopp();
+System.out.println(bike.wheels);
+System.out.println(bike.speed);
+
+}
+}
---
+++ Basic  inheritance/Vehicle.java
@@ -0,0 +1,16 @@
+public class Vehicle{
+
+double speed;
+
+// create go method 
+public void go(){
+	System.out.println("The vehicle is moving");
+}
+
+// create a stop method
+public void stopp(){
+	
+	System.out.println("The vehicle is stopping");
+}
+
+}
