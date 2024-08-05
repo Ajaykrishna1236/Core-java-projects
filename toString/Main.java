a9e27a8340375d520abf31a0d359afaf4b8fa72a
Ajay <Ajaykrishnachnr@gmail.com>
2024-08-05 13:17:00 +0530

    toString() project

---
+++ toString/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+public static void main(String[]args){
+// toString():toString method is a special method,that all object inherit,that return string value,can be use both implicitly and explicitly
+// create an object of Person
+Person person =new Person();
+// implicitly use
+System.out.println(person.toString());
+
+// explicitly using
+System.out.println();
+System.out.println(person);
+}
+}
---
+++ toString/Person.java
@@ -0,0 +1,14 @@
+public class Person{
+String name="Ajay Krishna";
+int age =24;
+String job ="java ceveloper";
+int salary =20000;
+double weight=70.5;
+
+// overide toString()
+public String toString(){
+	return name+"\n"+age+"\n"+salary+"\n"+weight;
+}
+
+
+}
