0caba4fdc21050f22ac6fe68153f31ca8df933c4
Ajay <Ajaykrishnachnr@gmail.com>
2024-08-09 12:25:35 +0530

    method chaining completed

---
+++ Method chaining/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+
+public static void main(String[]args){
+
+// method chaining =a syntax for invoking multiple methods calls in oop condense code into less lines
+
+String name="          Ajay";
+ //name=name.concat("krishna");
+ // name=name.toUpperCase();
+ //name=name.trim();
+ 
+ 
+ name=name.concat("krishna").toUpperCase().trim();
+System.out.println(name);
+
+}
+}
