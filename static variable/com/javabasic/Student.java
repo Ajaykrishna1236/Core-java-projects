fb490cbd1ea54f6bb2087783fcd42482eb85c085
Ajay <Ajaykrishnachnr@gmail.com>
2024-10-09 22:20:48 +0530

    static variable

---
+++ static variable/com/javabasic/Student.java
@@ -0,0 +1,13 @@
+package com.javabasic;
+public class Student{
+
+public String studentName;
+public int age;
+public static String classTeacher;
+
+public String toString(){
+	
+	return studentName+"\t:"+age+"\t:"+classTeacher;
+}
+
+}
---
+++ static variable/com/main/Main.java
@@ -0,0 +1,35 @@
+package com.main;
+import com.javabasic.*;
+public class Main{
+
+public static void main(String[]args){
+	// create object of student
+
+Student obj= new Student();
+obj.studentName="Ajay Krishna";
+obj.age=15;
+obj.classTeacher="Sathyan";
+
+
+Student obj1= new Student();
+obj1.studentName="Krishna";
+obj1.age=12;
+obj1.classTeacher="Sathyan";
+
+
+Student obj2= new Student();
+obj2.studentName="Krishna";
+obj2.age=12;
+obj2.classTeacher="madhavan";
+obj2.classTeacher="Varun Krishna";
+
+
+System.out.println(obj);
+
+System.out.println(obj1);
+
+
+System.out.println(obj2);
+}
+
+}
